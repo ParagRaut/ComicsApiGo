0caa6ffee5630876ebd78bf409fc2fd9c719f625
Parag <parag.raut@outlook.com>
2020-10-25 17:38:11 +0100

    corrected logic

--- comicsservice/main.go
+++ comicsservice/main.go
@@ -7,12 +7,10 @@ import (
 
 func main() {
 
-	data := service.GetComicURL()
-
 	r := gin.Default()
 	r.GET("/getComic", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"comicURL": data,
+			"comicURL": service.GetComicURL(),
 		})
 	})
 	r.Run()

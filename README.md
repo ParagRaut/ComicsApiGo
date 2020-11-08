40a0317f89dc3fd180eeb8e4a003cb1e8f6509cf
Parag <parag.raut@outlook.com>
2020-11-08 15:58:45 +0100

    changed structure

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 It's example implementation of garfield comic api in Go
 
 It uses PuerkitoBio/goquery for web scraping and Gin-Gonic to server http request
+
+To run this project go to src/ and run go run ./
--- src/main.go
+++ src/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"./service"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-
 	r := gin.Default()
+
 	r.GET("/getComic", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"comicURL": service.GetComicURL(),
+			"comicURL": GetComicURL(),
 		})
 	})
+
 	r.Run()
 }
